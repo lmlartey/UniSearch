394bfa84d0d842e90974bebbdd333c76d0762241
Aaron Nichie <aaronichie@aaronichie-Satellite-P105.(none)>
2011-06-27 11:32:37 +0000

    updated

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$','hackathon.views.hello'),
+    url(r'^current_time/$','hackathon.views.current_time'),
 )
