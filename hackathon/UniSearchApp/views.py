4ec3434b93f587a7b9b95a448ca6647ad6dd703e
Aaron Nichie <aaronichie@aaronichie-Satellite-P105.(none)>
2011-06-27 14:36:38 +0000

    erte

--- hackathon/UniSearchApp/views.py
+++ hackathon/UniSearchApp/views.py
@@ -10,3 +10,9 @@ def hello(request):
 def current_time(request):
 	now=datetime.datetime.now()
 	return render_to_response('base.html',{'current_date':now})
+
+def selectUniversity(reguest,id):
+	university=University.objects.get(pk=id)
+	t=loader.get_template('hackathon/lists.html')
+ 	c=Context({'university',university})
+	return HttpResponse(t.render(c))
