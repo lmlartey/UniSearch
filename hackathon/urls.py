a352d1d4a0b18a267bfc45cd7217986ae3bce330
Asante Kwadwo okrah <okrah.asante@yahoo.com>
2011-06-27 15:19:57 +0000

    changes again@ @

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^base/','hackathon.view.base'),
     url(r'^hello/$','hackathon.views.hello'),
     url(r'^time/$','hackathon.views.current_time'),
 )
